3945b5025e03f2dab4ceed2df652f2bf419ea198
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-06-08 15:14:06 +0100

    after eng meeting discussions and stuff + nodemon

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const isInServer = typeof window === "undefined";
 
-export default function App({name = "world!!"}) {
+export default function App({name = "world!!", users = []}) {
   return (
     <div>
       <span>Hello, {name}</span>
+      <form method="get" action="/greeting">
+        <input autoFocus name="name" placeholder="type a name"/>
+        <button type="submit">Go!</button>
+      </form>
+      <hr />
+      <ul>
+        {(users ?? []).map(t => <li key={t.id}>user {t.email}</li>)}
+      </ul>
     </div>
   );
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,7 @@
+import Greeting from "../client/Greeting";
+
 const React = require("react")
+import "isomorphic-fetch"
 const express = require("express")
 const ReactDOMServer = require("react-dom/server")
 import App from "../client/App"
@@ -6,18 +9,37 @@ import App from "../client/App"
 const app = express()
 
 const port = process.env.PORT ?? 3000
+
 const clientAppBaseUrl = process.env.CLIENT_APP_BASE_URL ?? 'http://localhost:8080'
 
 const clientBundleScript =
-  `<script src="${clientAppBaseUrl}/scripts/bundle.js"></script>`; 
+  `<script src="${clientAppBaseUrl}/scripts/bundle.js"></script>`;
 
 
-app.get('*', (_req, res) => {
-  const reactApp = ReactDOMServer.renderToString(<App />);
+app.get('*', async (request, res) => {
+  const {path, url} = request;
+
+  let reactApp = null;
+
+  let routeData = {};
+  if (path === "/greeting") {
+    const name = url.match(/\/greeting\?name=(.*?)$/i)?.[1];
+    reactApp = ReactDOMServer.renderToString(<Greeting name={name} />);
+  } else {
+    let users = await fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+
+    routeData = {users};
+    reactApp = ReactDOMServer.renderToString(<App users={users} />);
+  }
+
   return res.send(
     `
       <html>
         <head>
+            <script>
+                window.hydrationData = ${JSON.stringify(routeData)}
+            </script>
         </head>
         <body>
           <div id="root">${reactApp}</div>
@@ -31,3 +53,32 @@ app.get('*', (_req, res) => {
 app.listen(port, () => {
   console.log(`Started listening at http://localhost:${port}`)
 })
+
+// const app = express()
+//
+// app.get("*", (request, response) => {
+//
+//
+//   const parsedRoute = parseRoute(request);
+//   const currentUser = resolveUser(request);
+//   const {isSupported, isProtected, params} = parsedRoute;
+//
+//   const canSeeRoute = isAllowedToSeeRoute(currentUser, parsedRoute);
+//
+//   const name = request.params.name || "world!";
+//   console.log(`received a request to ${name}`)
+//
+//   response.send(`
+//     <html>
+//       <body>
+//         <p>
+//           Hello, ${name}
+//         </p>
+//       </body>
+//     </html>
+//   `);
+// })
+//
+// app.listen(4000, () => {
+//   console.log('APP STARTED http://localhost:4000')
+// })
